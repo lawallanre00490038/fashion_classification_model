9288b7c411a9974a62909159d6b076d6457ca1eb
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-07-08 10:55:40 +0100

    test

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer>
-        <div className="m-auto text-black rounded-md flex justify-between items-center px-4 font-semibold">   
+        <div className="lg:m-auto text-black rounded-md flex justify-between items-center px-1 sm:text-sm  md:px-4 font-semibold">   
             <div className="">
                 <h1>Image Classifier</h1>
             </div>
