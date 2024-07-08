5bc832529f8c1202e5eb2d109f851528ae55ccb9
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-07-08 10:32:42 +0100

    etst

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className='uppercase max-w-[1180px] m-auto text-white rounded-md flex justify-between items-center h-10 bg-blue-400 px-[10px] font-semibold'>
         <div className="">
-            <h1>Image Classifier</h1>
+            <h1>Friuts Classifier</h1>
         </div>
         <ul>
             <li>
