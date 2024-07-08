320f3ae8470a3fd22cf028f8e0c74aaada015bf0
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-07-08 10:37:30 +0100

    test

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='uppercase max-w-[1180px] m-auto text-white rounded-md flex justify-between items-center h-10 bg-blue-400 px-[10px] font-semibold'>
+    <nav className='uppercase max-w-[1180px] m-auto text-black bg-gray-100 rounded-md flex justify-between items-center h-10 shadow-sm border px-[10px] font-semibold'>
         <div className="">
             <h1>Friuts Classifier</h1>
         </div>
