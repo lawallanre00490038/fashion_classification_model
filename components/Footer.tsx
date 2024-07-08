d970e028d50f45a3b5399b061027f569a6f76df1
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-07-08 10:59:26 +0100

    test

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 function Footer() {
   return (
     <footer>
-        <div className="lg:m-auto text-black rounded-md flex justify-between items-center px-1 sm:text-sm  md:px-4 font-semibold">   
+        <div className="lg:m-auto text-center text-black rounded-md lg:flex justify-between items-center px-1 sm:text-sm  md:px-4 font-semibold">   
             <div className="">
                 <h1>Image Classifier</h1>
             </div>
-            <p className='font-normal'>
+            <p className='md:font-normal'>
                 &copy; {new Date().getFullYear()} Softmax Tech Solutions
             </p>
         </div>
