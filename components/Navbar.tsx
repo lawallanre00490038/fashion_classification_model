b4943e09660853a99047407385eba0afdcc7c318
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-07-08 10:53:54 +0100

    test

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,11 +5,11 @@ const Navbar = () => {
   return (
     <nav className='uppercase max-w-[1180px] m-auto text-black bg-gray-100 rounded-md flex justify-between items-center h-10 shadow-sm border px-[10px] font-semibold'>
         <div className="">
-            <h1>Friuts Classifier</h1>
+            <h1 className='text-green-600'>Friuts Classifier</h1>
         </div>
         <ul>
             <li>
-                <Link href="/">Home</Link>
+                <Link href="/" className=''>Home</Link>
             </li>
         </ul>
     </nav>
